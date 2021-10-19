558d0074747ef351445915b93d0f1426091719cc
boxer <aszzoon@naver.com>
2021-10-19 11:59:27 +0900

    js navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-"use strict";
-
-//Make navbar transparent when
+//Make navbar transparent whe
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -11,4 +9,18 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// handel scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar_menu");
+navbarMenu.addEventListener("click", event => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
